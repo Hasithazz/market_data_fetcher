f2d0e0eeabf64b923b6eaf0a9fbe29cbd20915a9
Hasitha Edirisinghe <hsedirisingha@gmail.com>
2024-12-21 13:15:32 +0530

    Folder structure changed .env file added logger added

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const config = require('./config');
+const config = require('./config/config');
+const logger = require('./utils/logger');
 
 const app = express();
 
 app.listen(config.port, () => {
-    console.log(`Listening on port ${config.port}`)
+    console.log(`Listening on port ${config.port}`);
+    logger.info(`Server Started on port ${config.port}`);
 })
